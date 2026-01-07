d4616576515dd625efe859a7dee86fb5f0aa6b6e
marq <classicvector63@gmail.com>
2026-01-07 11:22:11 +0530

    resolve creating string II issue

--- Mathematics/11.Creating Strings II.cpp
+++ Mathematics/11.Creating Strings II.cpp
@@ -24,7 +24,7 @@ int main(){
     }
     string s; cin>>s;
     ll n = s.size();
-    unordered_map<int,int> mp;
+    unordered_map<char,int> mp;
     for(int i =0 ;i<n; i++){
         mp[s[i]]++;
     }
